fa4e00bad02a2390b587232893da44351e0e83c1
AWS ParallelCluster user <ubuntu@ip-10-0-0-224.us-east-2.compute.internal>
2021-02-10 02:46:45 +0000

    added batch script

---
+++ batch_run_covid.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+S3DOWNLOAD=$1
+
+for SAMPLE in $(aws s3 ls $S3DOWNLOAD/  --recursive | grep fastq | awk '{print $4}' | awk -F '/|_R' '{print $5}' | sort | uniq | grep -v Undetermined); do
+	qsub -v SAMPLE=$SAMPLE \
+		 -v S3DOWNLOAD=$S3DOWNLOAD \
+		 -N $SAMPLE \
+		 -wd /shared/workspace/projects/covid/logs \
+		 -pe smp 1 \
+		 -S /bin/bash \
+		 /shared/workspace/software/covid_sequencing_analysis_pipeline/covid.sh
+done
