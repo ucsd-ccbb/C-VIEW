0d79555e761009ce0ab2fab1f792d28227a3d582
AWS ParallelCluster user <ubuntu@ip-10-0-0-224.us-east-2.compute.internal>
2021-02-10 02:45:15 +0000

    removed prep_reference.sh

--- prep_reference.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# prep reference
-minimap2 -t THREADS -d NC045512.fas.mmi NC045512.fas
-minimap2 -t THREADS -a -x map-ont REFERENCE_GENOME.FAS.MMI PRIMERS.FAS | samtools view -b -F 4 > PRIMERS.BAM
-bedtools bamtobed -i PRIMERS.BAM > PRIMERS.BED
