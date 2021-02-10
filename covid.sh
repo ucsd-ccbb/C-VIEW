7e3ff6af5deb84823cce6a786af72605803c292f
AWS ParallelCluster user <ubuntu@ip-10-0-0-224.us-east-2.compute.internal>
2021-02-10 02:42:27 +0000

    working covid pipeline

--- covid.sh
+++ covid.sh
@@ -1,46 +1,55 @@
 #!/bin/bash
 
-# Step 1: Map Reads and Sort
-# Input: FASTQ (or FASTQ.GZ) file(s) (X.fastq or X.fastq.gz)
-# Output: Sorted Untrimmed BAM (X.sorted.bam)
-minimap2 -t THREADS -a -x map-ont ../ref/NC_045512.2.fas.mmi READ1.FASTQ.GZ READ2.FASTQ.GZ | samtools sort --threads THREADS -o SORTED.BAM
+export PATH=/shared/workspace/software/SD-COVID-Sequencing:/shared/workspace/software/ivar/bin:/shared/workspace/software/anaconda3/bin:$PATH
 
-# Step 2: Trim Sorted BAM (resulting in unsorted trimmed BAM)
-# Input: Sorted Untrimmed BAM (X.sorted.bam)
-# Output: Unsorted Trimmed BAM (X.trimmed.bam)
-ivar trim -e -i SORTED.BAM -b PRIMERS.bed -p TRIMMED_PREFIX
+THREADS=1
+REF_FAS="/scratch/reference/NC_045512.2.fas"
+REF_MMI="/scratch/reference/NC_045512.2.fas.mmi"
+REF_GFF="/scratch/reference/NC_045512.2.gff3"
+PRIMER_BED="/scratch/reference/sarscov2_v2_primers.bed"
+WORKSPACE=/scratch/$SAMPLE
+mkdir -p $WORKSPACE
+
+if [[ ! -f "$REF_FAS" ]]; then
+	mkdir -p /scratch/reference/
+    cp /shared/workspace/software/SD-COVID-Sequencing/reference_genome/NC_045512.2.fas $REF_FAS
+    cp /shared/workspace/software/SD-COVID-Sequencing/reference_genome/NC_045512.2.fas.mmi $REF_MMI
+    cp /shared/workspace/software/SD-COVID-Sequencing/reference_genome/NC_045512.2.gff3 $REF_GFF
+    cp /shared/workspace/software/SD-COVID-Sequencing/primers/swift/sarscov2_v2_primers.bed $PRIMER_BED
+fi
+
+# Step 0: Download fastq
+aws s3 cp $S3DOWNLOAD/"$SAMPLE"_R1_001.fastq.gz $WORKSPACE/
+aws s3 cp $S3DOWNLOAD/"$SAMPLE"_R2_001.fastq.gz $WORKSPACE/
+
+
+# Step 1: Map Reads + Sort
+{ time ( minimap2 -t $THREADS -a -x sr $REF_MMI $WORKSPACE/"$SAMPLE"*.fastq.gz | samtools sort --threads $THREADS -o $WORKSPACE/"$SAMPLE".sorted.bam ) ; } 2> $WORKSPACE/"$SAMPLE".log.1.map.log
+
+# Step 2: Trim Sorted BAM
+{ time ( ivar trim -x 5 -e -i $WORKSPACE/"$SAMPLE".sorted.bam -b $PRIMER_BED -p $WORKSPACE/"$SAMPLE".trimmed ) ; } > $WORKSPACE/"$SAMPLE".log.2.trim.log 2>&1
 
 # Step 3: Sort Trimmed BAM
-# Input: Unsorted Trimmed BAM (X.trimmed.bam)
-# Output: Sorted Trimmed BAM (X.trimmed.sorted.bam)
-samtools sort --threads THREADS -o TRIMMED_SORTED.BAM TRIMMED_PREFIX.BAM #&& rm TRIMMED_PREFIX.BAM
+{ time ( samtools sort --threads $THREADS -o $WORKSPACE/"$SAMPLE".trimmed.sorted.bam $WORKSPACE/"$SAMPLE".trimmed.bam && rm $WORKSPACE/"$SAMPLE".trimmed.bam ) ; } 2> $WORKSPACE/"$SAMPLE".log.3.sorttrimmed.log
 
-# Step 4: Generate Pile-Up from Trimmed Sorted BAM
-# Input: Sorted Trimmed BAM (X.trimmed.sorted.bam)
-# Output: Pile-up (X.pileup.txt or X.pileup.txt.gz)
-samtools mpileup -A -aa -d 0 -Q 0 --reference REFERENCE.FAS TRIMMED_SORTED.BAM > PILEUP.TXT
+# Step 4: Generate Pile-Up
+{ time ( samtools mpileup -A -aa -d 0 -Q 0 --reference $REF_FAS $WORKSPACE/"$SAMPLE".trimmed.sorted.bam ) ; } > $WORKSPACE/"$SAMPLE".trimmed.sorted.pileup.txt 2> $WORKSPACE/"$SAMPLE".log.4.pileup.log
 
-# Step 5: Call Variants from Pile-Up
-# Input: Pile-up (X.pileup.txt or X.pileup.txt.gz)
-# Output: Variants (X.variants.tsv)
-cat PILEUP.TXT | ivar variants -r REFERENCE.FAS -g REFERENCE.GFF -p VARIANTS.TSV -m 10
+# Step 5: Call Variants
+{ time ( cat $WORKSPACE/"$SAMPLE".trimmed.sorted.pileup.txt | ivar variants -r $REF_FAS -g $REF_GFF -p $WORKSPACE/"$SAMPLE".trimmed.sorted.pileup.variants.tsv -m 10 ) ; } 2> $WORKSPACE/"$SAMPLE".log.5.variants.log
 
-# Step 6: Call Consensus Sequence from Pile-Up
-# Input: Pile-up (X.pileup.txt or X.pileup.txt.gz)
-# Ouptut: Consensus Sequence (X.consensus.fas or X.consensus.fas.gz)
-cat PILEUP.TXT | ivar consensus -p CONSENSUS.FAS -m 10 -n N -t 0.5
+# Step 6: Call Consensus
+{ time ( cat $WORKSPACE/"$SAMPLE".trimmed.sorted.pileup.txt | ivar consensus -p $WORKSPACE/"$SAMPLE".trimmed.sorted.pileup.consensus -m 10 -n N -t 0.5 ) ; } > $WORKSPACE/"$SAMPLE".log.6.consensus.log 2>&1
 
-# Step 7: Call Depth (supplemental summary stats)
-# Input: Sorted Trimmed BAM (X.trimmed.sorted.bam)
-# Output: Depth (X.trimmed.sorted.depth.txt)
-samtools depth -d 0 -Q 0 -q 0 -aa TRIMMED_SORTED.BAM > DEPTH.TXT
+# Step 7: Call Depth
+{ time ( samtools depth -d 0 -Q 0 -q 0 -aa $WORKSPACE/"$SAMPLE".trimmed.sorted.bam ) ; } > $WORKSPACE/"$SAMPLE".trimmed.sorted.depth.txt 2> $WORKSPACE/"$SAMPLE".log.7.depth.log
 
-# Step 8: Qualimap Report (supplemental summary stats)
-# Input: Sorted BAM (X.sorted.bam) and Sorted Trimmed BAM (X.trimmed.sorted.bam)
-# Output: Qualimap Reports (X.sorted.stats.tar.gz and X.trimmed.sorted.stats.tar.gz)
-qualimap bamqc -bam MAPPING.BAM -nt THREADS --java-mem-size=RAM -outdir STATS_DIR && tar c STATS_DIR | pigz -9 -p THREADS > STATS_DIR.tar.gz && rm -rf STATS_DIR
-# Consolidating Stats
-#qualimap_targz_to_TSV.py *.trimmed.sorted.stats.tar.gz > YYYY-MM-DD.trimmed.sorted.stats.tsv
+# # Step 8: Qualimap
+for x in sorted trimmed.sorted ; do
+    { time ( qualimap bamqc -bam $WORKSPACE/"$SAMPLE".$x.bam -nt $THREADS --java-mem-size=4G -outdir $WORKSPACE/"$SAMPLE".$x.stats && tar c $WORKSPACE/"$SAMPLE".$x.stats | pigz -9 -p $THREADS > $WORKSPACE/"$SAMPLE".$x.stats.tar.gz && rm -rf $WORKSPACE/"$SAMPLE".$x.stats ) ; } > $WORKSPACE/"$SAMPLE".log.8.qualimap.$x.log 2>&1
+done
 
-# Archiving Each Sample's Files
-zip -9 SAMPLE.zip SAMPLE*
+# Step 9: Zip
+cd $WORKSPACE && zip -9 "$SAMPLE".zip "$SAMPLE"*
+
+aws s3 cp $WORKSPACE/"$SAMPLE".zip $S3DOWNLOAD/results/
