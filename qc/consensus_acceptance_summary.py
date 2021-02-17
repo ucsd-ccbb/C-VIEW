38b03186c0bda4a1bbbd5e1bd55da90ff421fcb5
AmandaBirmingham <abirmingham@ucsd.edu>
2021-02-17 14:50:22 -0800

    modified to pass in directory in which to look for *.acceptance.tsv files instead of just looking in working directory

--- qc/consensus_acceptance_summary.py
+++ qc/consensus_acceptance_summary.py
@@ -1,20 +1,29 @@
 import os
-from sys import exit
+from sys import argv, stderr
 
 
 ACCEPTANCE_SUFFIX = ".acceptance.tsv"
 header_line = None
 output_lines = []
-output_fp = f"summary{ACCEPTANCE_SUFFIX}"
-curr_dir_contents = os.listdir(".")
+output_fname = f"summary{ACCEPTANCE_SUFFIX}"
+USAGE = "USAGE: %s <workspace directory>" % argv[0]
+
+# check command line for validity
+if len(argv) != 2:
+    print(USAGE, file=stderr)
+    exit(1)
+
+workspace_dir = argv[1]
+curr_dir_contents = os.listdir(workspace_dir)
 for curr_item_name in sorted(curr_dir_contents):
     # ignore any previous output of this script and ignore any file that
     # does not look like a per-sample acceptance file
-    if curr_item_name == output_fp or \
+    if curr_item_name == output_fname or \
             not curr_item_name.endswith(".acceptance.tsv"):
         continue
 
-    with open(curr_item_name) as curr_acceptance_f:
+    curr_item_fp = os.path.join(workspace_dir, curr_item_name)
+    with open(curr_item_fp) as curr_acceptance_f:
         curr_acceptance_lines = curr_acceptance_f.readlines()
         curr_header_line = curr_acceptance_lines[0]
 
@@ -32,5 +41,6 @@ if len(output_lines) == 0:
     print("Error: No acceptance summary info found")
     exit(1)
 
+output_fp = os.path.join(workspace_dir, output_fname)
 with open(output_fp, "w") as output_f:
     output_f.writelines(output_lines)
