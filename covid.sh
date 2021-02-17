6cd652645e5ed7fc33845dd9c89f094eae8a3d90
AWS ParallelCluster user <ubuntu@ip-10-0-0-224.us-east-2.compute.internal>
2021-02-17 01:20:00 +0000

    paramterized covid.sh

--- covid.sh
+++ covid.sh
@@ -2,30 +2,64 @@
 
 export PATH=/shared/workspace/software/ivar/bin:/shared/workspace/software/anaconda3/envs/covid1.1/bin:$PATH
 
+# Set variables
 THREADS=1
+WORKSPACE=/scratch/$SAMPLE
+PIPELINEDIR=/shared/workspace/software/covid_sequencing_analysis_pipeline
 REF_FAS="/scratch/reference/NC_045512.2.fas"
 REF_MMI="/scratch/reference/NC_045512.2.fas.mmi"
 REF_GFF="/scratch/reference/NC_045512.2.gff3"
-PRIMER_BED="/scratch/reference/nCoV-2019.primer.bed"
-WORKSPACE=/scratch/$SAMPLE
-mkdir -p $WORKSPACE/fastqc
 
+# Move reference files to compute node once
 if [[ ! -f "$REF_FAS" ]]; then
 	mkdir -p /scratch/reference/
-    cp /shared/workspace/software/SD-COVID-Sequencing/reference_genome/NC_045512.2.fas $REF_FAS
-    cp /shared/workspace/software/SD-COVID-Sequencing/reference_genome/NC_045512.2.fas.mmi $REF_MMI
-    cp /shared/workspace/software/SD-COVID-Sequencing/reference_genome/NC_045512.2.gff3 $REF_GFF
-    cp /shared/workspace/projects/covid/data/primers/nCoV-2019.primer.bed $PRIMER_BED
+    cp $PIPELINEDIR/reference_files/NC_045512.2.fas $REF_FAS
+    cp $PIPELINEDIR/reference_files/NC_045512.2.fas.mmi $REF_MMI
+    cp $PIPELINEDIR/reference_files/NC_045512.2.gff3 $REF_GFF
 fi
 
+if [[ "$IS_ARTIC" == true ]]; then
+	PRIMER_BED="/scratch/reference/nCoV-2019.primer.bed"
+	if [[ ! -f "$PRIMER_BED" ]]; then
+		cp $PIPELINEDIR/reference_files/nCoV-2019.primer.bed $PRIMER_BED
+	fi
+else
+	PRIMER_BED="/scratch/reference/sarscov2_v2_primers.bed"
+	if [[ ! -f "$PRIMER_BED" ]]; then
+		cp $PIPELINEDIR/reference_files/sarscov2_v2_primers.bed $PRIMER_BED
+	fi
+fi
+
+# Clear fastq directory if node is being reused
+rm -rf $WORKSPACE/*
+mkdir -p $WORKSPACE/fastq
+mkdir -p $WORKSPACE/fastqc
+
 # Step 0: Download fastq
-aws s3 cp $S3DOWNLOAD/ $WORKSPACE/ --recursive --exclude "*" --include "$SAMPLE*fastq.gz"
+RESULTS=results_"$MERGED"_"$FQ"
+
+if [[ "$RESULTS" == results_merged_se ]]; then
+	aws s3 cp $S3DOWNLOAD/ $WORKSPACE/ --recursive --exclude "*" --include "$SAMPLE*"
+	cat $WORKSPACE/"$SAMPLE"*R1_001.fastq.gz > $WORKSPACE/fastq/"$SAMPLE"_R1_001.fastq.gz
+
+elif [[ "$RESULTS" == results_merged_pe ]]; then
+	aws s3 cp $S3DOWNLOAD/ $WORKSPACE/ --recursive --exclude "*" --include "$SAMPLE*"
+	cat $WORKSPACE/"$SAMPLE"*R1_001.fastq.gz > $WORKSPACE/fastq/"$SAMPLE"_R1_001.fastq.gz
+	cat $WORKSPACE/"$SAMPLE"*R2_001.fastq.gz > $WORKSPACE/fastq/"$SAMPLE"_R2_001.fastq.gz
+
+elif [[ "$RESULTS" == results_unmerged_se ]]; then
+	aws s3 cp $S3DOWNLOAD/"$SAMPLE"_R1_001.fastq.gz $WORKSPACE/fastq/
+
+elif [[ "$RESULTS" == results_unmerged_pe ]]; then
+	aws s3 cp $S3DOWNLOAD/"$SAMPLE"_R1_001.fastq.gz $WORKSPACE/fastq/
+	aws s3 cp $S3DOWNLOAD/"$SAMPLE"_R2_001.fastq.gz $WORKSPACE/fastq/
+fi
 
 # Fastqc
-fastqc $WORKSPACE/"$SAMPLE"*fastq.gz -o $WORKSPACE/fastqc
+fastqc $WORKSPACE/fastq/"$SAMPLE"*fastq.gz -o $WORKSPACE/fastqc
 
 # Step 1: Map Reads + Sort
-{ time ( minimap2 -t $THREADS -a -x sr $REF_MMI $WORKSPACE/"$SAMPLE"*.fastq.gz | samtools sort --threads $THREADS -o $WORKSPACE/"$SAMPLE".sorted.bam ) ; } 2> $WORKSPACE/"$SAMPLE".log.1.map.log
+{ time ( minimap2 -t $THREADS -a -x sr $REF_MMI $WORKSPACE/fastq/"$SAMPLE"*.fastq.gz | samtools sort --threads $THREADS -o $WORKSPACE/"$SAMPLE".sorted.bam ) ; } 2> $WORKSPACE/"$SAMPLE".log.1.map.log
 
 # Step 2: Trim Sorted BAM
 { time ( ivar trim -x 5 -e -i $WORKSPACE/"$SAMPLE".sorted.bam -b $PRIMER_BED -p $WORKSPACE/"$SAMPLE".trimmed ) ; } > $WORKSPACE/"$SAMPLE".log.2.trim.log 2>&1
@@ -46,22 +80,10 @@ fastqc $WORKSPACE/"$SAMPLE"*fastq.gz -o $WORKSPACE/fastqc
 { time ( samtools depth -d 0 -Q 0 -q 0 -aa $WORKSPACE/"$SAMPLE".trimmed.sorted.bam ) ; } > $WORKSPACE/"$SAMPLE".trimmed.sorted.depth.txt 2> $WORKSPACE/"$SAMPLE".log.7.depth.log
 
 # # Step 8: Qualimap
-for x in sorted trimmed.sorted ; do
-    { time ( qualimap bamqc -bam $WORKSPACE/"$SAMPLE".$x.bam -nt $THREADS --java-mem-size=4G -outdir $WORKSPACE/"$SAMPLE".$x.stats && tar c $WORKSPACE/"$SAMPLE".$x.stats | pigz -9 -p $THREADS > $WORKSPACE/"$SAMPLE".$x.stats.tar.gz && rm -rf $WORKSPACE/"$SAMPLE".$x.stats ) ; } > $WORKSPACE/"$SAMPLE".log.8.qualimap.$x.log 2>&1
-done
+{ time ( qualimap bamqc -bam $WORKSPACE/"$SAMPLE".sorted.bam -nt $THREADS --java-mem-size=4G -outdir $WORKSPACE/"$SAMPLE".sorted.stats ) ; } > $WORKSPACE/"$SAMPLE".log.8.qualimap.$x.log 2>&1
 
 # QC
-python /shared/workspace/software/covid_sequencing_analysis_pipeline/test_sarscov2_consensus_qc.py $WORKSPACE/"$SAMPLE".trimmed.sorted.pileup.consensus.fa $WORKSPACE/"$SAMPLE".trimmed.sorted.depth.txt $REF_FAS
+python $PIPELINEDIR/sarscov2_consensus_acceptance.py $WORKSPACE/"$SAMPLE".trimmed.sorted.pileup.consensus.fa $WORKSPACE/"$SAMPLE".trimmed.sorted.depth.txt $REF_FAS
 
-# Step 9: Zip
-cd $WORKSPACE && zip -9 "$SAMPLE".zip "$SAMPLE"*
+aws s3 cp $WORKSPACE/ $S3DOWNLOAD/$RESULTS/$SAMPLE/ --recursive --include "*" --exclude "*fastq.gz"
 
-aws s3 cp $WORKSPACE/"$SAMPLE".zip $S3DOWNLOAD/results_20210212/zip/
-aws s3 cp $WORKSPACE/"$SAMPLE".trimmed.sorted.pileup.variants.tsv $S3DOWNLOAD/results_20210212/variants/
-aws s3 cp $WORKSPACE/"$SAMPLE".trimmed.sorted.pileup.consensus.fa $S3DOWNLOAD/results_20210212/consensus/
-aws s3 cp $WORKSPACE/"$SAMPLE".trimmed.sorted.depth.txt $S3DOWNLOAD/results_20210212/depth/
-aws s3 cp $WORKSPACE/"$SAMPLE".sorted.stats.tar.gz $S3DOWNLOAD/results_20210212/qualimap/
-aws s3 cp $WORKSPACE/"$SAMPLE".trimmed.sorted.stats.tar.gz $S3DOWNLOAD/results_20210212/qualimap/
-aws s3 cp $WORKSPACE/fastqc/ $S3DOWNLOAD/results_20210212/fastqc/ --recursive
-aws s3 cp $WORKSPACE/ $S3DOWNLOAD/results_20210212/logs/ --recursive --exclude "*" --include "*log"
-aws s3 cp $WORKSPACE/"$SAMPLE".passfail.tsv $S3DOWNLOAD/results_20210212/qc
