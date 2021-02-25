a16bb26dc2e04a06b93cc9e82f3bc8760b9cd39d
AmandaBirmingham <abirmingham@ucsd.edu>
2021-02-25 09:42:42 -0800

    replaced whole-hog with submit_merge_pipeline_qc.sh contents bc that script is in fact the update of this one and I want to see diffs

--- pipeline/batch_submission.sh
+++ pipeline/batch_submission.sh
@@ -3,16 +3,21 @@
 INPUT=$1 # Sample Sheet with header - batch,s3download,s3upload,primers,reads
 TIMESTAMP=$(date +'%Y-%m-%d_%H-%M-%S')
 PIPELINEDIR=/shared/workspace/software/covid_sequencing_analysis_pipeline
+QSUBSAMPLEPARAMS=''
 
 [ ! -f $INPUT ] && { echo "Error: $INPUT file not found"; exit 99; }
-sed 1d $INPUT | while IFS=',' read BATCH S3DOWNLOAD S3UPLOAD PRIMER_SET FQ
+sed 1d $INPUT | while IFS=',' read BATCH S3DOWNLOAD S3UPLOAD PRIMER_SET FQ MERGE_LANES
 do
 	echo "Batch: $BATCH"
 	echo "S3 Fastq path: $S3DOWNLOAD"
 	echo "S3 Results Path: $S3UPLOAD"
 	echo "Primers: $PRIMER_SET"
 	echo "Fastq Reads: $FQ"
+	echo "Merge Lanes: $MERGE_LANES"
 
+	WORKSPACE=/scratch/$BATCH/$TIMESTAMP
+	# Append Results URL
+	RESULTS="$TIMESTAMP"_"$FQ"
 
 	if [[ ! "$PRIMER_SET" =~ ^(artic|swift_v2)$ ]]; then
 		echo "Error: Parameter PRIMER_SET must be one of artic or swift_v2"
@@ -24,22 +29,56 @@ do
 	  exit 1
 	fi
 
+	if [[ ! "$MERGE_LANES" =~ ^(true|false)$ ]]; then
+	  echo "Error: MERGE_LANES must be one of true or false"
+	  exit 1
+	fi
+
+	# Merge fastq files from multiple lanes
+	if [[ "$MERGE_LANES" == true ]]; then
+		qsub -v BATCH=$BATCH \
+			 -v WORKSPACE=$WORKSPACE \
+			 -v S3DOWNLOAD=$S3DOWNLOAD \
+			 -wd /shared/workspace/projects/covid/logs \
+			 -N merge_lanes_"$BATCH" \
+			 -pe smp 16 \
+			 $PIPELINEDIR/pipeline/merge_lanes.sh
+		S3DOWNLOAD=$S3DOWNLOAD/merged_lanes
+		S3UPLOAD=$S3UPLOAD/merged_lanes
+		QSUBSAMPLEPARAMS=' -hold_jid merge_lanes_'$BATCH''
+	fi
 
 	SAMPLE_LIST=$(aws s3 ls $S3DOWNLOAD/ | grep fastq | awk '{print $NF}' | awk -F '_R' '{print $1}' | sort | uniq | grep -v Undetermined)
 
+	# Run pipeline on each sample
 	for SAMPLE in $SAMPLE_LIST; do
-		qsub -v SAMPLE=$SAMPLE \
+		qsub $QSUBSAMPLEPARAMS \
+			-v SAMPLE=$SAMPLE \
 			-v S3DOWNLOAD=$S3DOWNLOAD \
-			-v S3UPLOAD=$S3UPLOAD \
+			-v S3UPLOAD=$S3UPLOAD/$RESULTS \
 			-v PRIMER_SET=$PRIMER_SET \
 			-v FQ=$FQ \
 			-v TIMESTAMP=$TIMESTAMP \
-			-N Covid19_"$SAMPLE" \
+			-N Covid19_"$BATCH"_"$SAMPLE" \
 			-wd /shared/workspace/projects/covid/logs \
 			-pe smp 1 \
 			-S /bin/bash \
 			$PIPELINEDIR/pipeline/sarscov2_consensus_pipeline.sh
 	done
+
+
+	# Perform QC and summary on batch when all samples have completed
+	qsub \
+		-hold_jid 'Covid19_'$BATCH'_*' \
+		-v S3DOWNLOAD=$S3UPLOAD/$RESULTS \
+		-v S3UPLOAD=$S3UPLOAD/$RESULTS \
+		-v BATCH=$BATCH \
+		-v WORKSPACE=$WORKSPACE \
+		-N QC_summary_"$BATCH" \
+		-wd /shared/workspace/projects/covid/logs \
+		-pe smp 32 \
+		-S /bin/bash \
+    	$PIPELINEDIR/qc/qc_summary.sh
 
 	echo "batch,s3download,s3upload,primers,reads" > "$BATCH"-"$TIMESTAMP".csv
 	echo "$BATCH,$S3DOWNLOAD,$S3UPLOAD,$PRIMER_SET,$FQ" >> "$BATCH"-"$TIMESTAMP".csv
