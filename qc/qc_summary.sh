58acb427087bccf2e5137e59e7f6071d38802cf0
AmandaBirmingham <abirmingham@ucsd.edu>
2021-03-01 11:30:21 -0800

    remove commented-out lines for work now performed elsewhere

--- qc/qc_summary.sh
+++ qc/qc_summary.sh
@@ -45,22 +45,6 @@ runQC () {
 	cat $PIPELINEDIR/qc/covid_custom_config.yaml $WORKSPACE/multiqc_custom_gen_stats.yaml > $WORKSPACE/qc/"$SEQ_RUN"-custom_gen_stats_config.yaml
 	multiqc --config $WORKSPACE/qc/"$SEQ_RUN"-custom_gen_stats_config.yaml --module qualimap $WORKSPACE
 
-	# # Make merged consensus of passing samples
-	# cat $WORKSPACE/*.consensus.fa > $WORKSPACE/"$SEQ_RUN".fas
-	# # filter the true samples
-	# awk '{ if ($2 == "True") { print } }' $WORKSPACE/"$SEQ_RUN"-summary.acceptance.tsv > $WORKSPACE/"$SEQ_RUN"-summary.acceptance.true.tsv
-	# awk '{print $1}' $WORKSPACE/"$SEQ_RUN"-summary.acceptance.true.tsv > $WORKSPACE/"$SEQ_RUN"-passQC.samples.tsv
-
-	# # loop over individual .fa files, keep the ones which are in passQC.samples.tsv
-	# touch $WORKSPACE/"$SEQ_RUN"-passQC.fas # initialize the file
-	# for f in *.fa; do
-	#     fshort="$(cut -d'.' -f1 <<<$f)"
-	#     echo $fshort
-	#     if (grep -qF $fshort $WORKSPACE/"$SEQ_RUN"-passQC.samples.tsv); then
-	#        cat $f >> $WORKSPACE/"$SEQ_RUN"-passQC.fas
-	#     fi
-	# done
-
 	# Pangolin
 	cat $WORKSPACE/*.consensus.fa > $WORKSPACE/"$SEQ_RUN".fas
 	bash $PIPELINEDIR/qc/pangolin.sh $WORKSPACE/$SEQ_RUN
