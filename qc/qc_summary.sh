6f74d3e8650f105db12d2df7a192c465755902bc
AWS ParallelCluster user <ubuntu@ip-10-0-0-224.us-east-2.compute.internal>
2021-03-04 00:49:40 +0000

    added pangolin run into qc_summary.sh

--- qc/qc_summary.sh
+++ qc/qc_summary.sh
@@ -45,24 +45,9 @@ runQC () {
 	cat $PIPELINEDIR/qc/covid_custom_config.yaml $WORKSPACE/multiqc_custom_gen_stats.yaml > $WORKSPACE/qc/"$SEQ_RUN"-custom_gen_stats_config.yaml
 	multiqc --config $WORKSPACE/qc/"$SEQ_RUN"-custom_gen_stats_config.yaml --module qualimap $WORKSPACE
 
-	# # Make merged consensus of passing samples
-	cat $WORKSPACE/*.consensus.fa > $WORKSPACE/"$SEQ_RUN".fas
-	# filter the true samples
-	awk '{ if ($2 == "True") { print } }' $WORKSPACE/"$SEQ_RUN"-summary.acceptance.tsv > $WORKSPACE/"$SEQ_RUN"-summary.acceptance.true.tsv
-	awk '{print $1}' $WORKSPACE/"$SEQ_RUN"-summary.acceptance.true.tsv > $WORKSPACE/"$SEQ_RUN"-passQC.samples.tsv
-
-	# loop over individual .fa files, keep the ones which are in passQC.samples.tsv
-	touch $WORKSPACE/"$SEQ_RUN"-passQC.fas # initialize the file
-	for f in *.fa; do
-	    fshort="$(cut -d'.' -f1 <<<$f)"
-	    echo $fshort
-	    if (grep -qF $fshort $WORKSPACE/"$SEQ_RUN"-passQC.samples.tsv); then
-	       cat $f >> $WORKSPACE/"$SEQ_RUN"-passQC.fas
-	    fi
-	done
-
 	# Pangolin
-	#bash $PIPELINEDIR/qc/pangolin.sh $WORKSPACE/$SEQ_RUN
+	cat $WORKSPACE/*.consensus.fa > $WORKSPACE/"$SEQ_RUN".fas
+	bash $PIPELINEDIR/qc/pangolin.sh $WORKSPACE/$SEQ_RUN
 
 	# Make QC table
 	python $PIPELINEDIR/qc/makeQCSummaryTable.py $WORKSPACE/multiqc_data/multiqc_general_stats.txt $WORKSPACE/"$SEQ_RUN"-summary.acceptance.tsv
@@ -78,19 +63,19 @@ runQC () {
 	aws s3 cp $WORKSPACE/"$SEQ_RUN"-passQC.fas $PHYLORESULTS/
 	aws s3 cp $WORKSPACE/"$SEQ_RUN".fas $PHYLORESULTS/
 	aws s3 cp $WORKSPACE/"$SEQ_RUN"-summary.acceptance.tsv $PHYLORESULTS/
-	#aws s3 cp $WORKSPACE/"$SEQ_RUN".lineage_report.csv $PHYLORESULTS/
+	aws s3 cp $WORKSPACE/"$SEQ_RUN".lineage_report.csv $PHYLORESULTS/
 
 	# quality control folder
 	aws s3 cp $WORKSPACE/multiqc_data/ $QCRESULTS/"$SEQ_RUN"_multiqc_data/ --recursive --quiet
 	aws s3 cp $WORKSPACE/multiqc_report.html $QCRESULTS/"$SEQ_RUN"_multiqc_report.html
 	aws s3 cp $WORKSPACE/qc/ $QCRESULTS/ --recursive --quiet
-  aws s3 cp $WORKSPACE/"$SEQ_RUN"-QCSummaryTable.csv $QCRESULTS/
+    aws s3 cp $WORKSPACE/"$SEQ_RUN"-QCSummaryTable.csv $QCRESULTS/
 	aws s3 cp $WORKSPACE/"$SEQ_RUN"-summary.acceptance.tsv $QCRESULTS/
 
 	# Tree building data
 	aws s3 cp $WORKSPACE/"$SEQ_RUN"-passQC.fas s3://ucsd-ccbb-projects/2021/20210208_COVID_sequencing/tree_building/consensus/
 	aws s3 cp $WORKSPACE/"$SEQ_RUN"-summary.acceptance.tsv s3://ucsd-ccbb-projects/2021/20210208_COVID_sequencing/tree_building/acceptance/
-  aws s3 cp $WORKSPACE/"$SEQ_RUN"-QCSummaryTable.csv s3://ucsd-ccbb-projects/2021/20210208_COVID_sequencing/tree_building/qc_summary/
+    aws s3 cp $WORKSPACE/"$SEQ_RUN"-QCSummaryTable.csv s3://ucsd-ccbb-projects/2021/20210208_COVID_sequencing/tree_building/qc_summary/
 }
 
 { time ( runQC ) ; } > $WORKSPACE/qc/"$SEQ_RUN"-qc_summary.log 2>&1
