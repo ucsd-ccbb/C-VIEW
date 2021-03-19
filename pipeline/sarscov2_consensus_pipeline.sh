95828cca83bddeac3b4b252fffccd822e41b9800
AWS ParallelCluster user <ubuntu@ip-10-0-2-230.us-west-2.compute.internal>
2021-03-19 17:23:29 +0000

    threads=3

--- pipeline/sarscov2_consensus_pipeline.sh
+++ pipeline/sarscov2_consensus_pipeline.sh
@@ -5,7 +5,7 @@ export PATH=/shared/workspace/software/ivar/bin:$PATH
 ANACONDADIR=/shared/workspace/software/anaconda3/bin
 source $ANACONDADIR/activate covid1.2
 # Set variables
-THREADS=1
+THREADS=3
 NUM_READS_PLUS_3=1000003
 WORKSPACE=/scratch/$SAMPLE
 PIPELINEDIR=/shared/workspace/software/covid_sequencing_analysis_pipeline
@@ -60,10 +60,10 @@ if [[ "$FQ" == pe ]]; then
 fi
 
 # Fastqc
-{ time ( fastqc -t 3 $WORKSPACE/fastq/"$SAMPLE"*fastq.gz -o $WORKSPACE/fastqc ) ; } 2> $WORKSPACE/"$SAMPLE".log.0.fastqc.log
+{ time ( fastqc -t $THREADS $WORKSPACE/fastq/"$SAMPLE"*fastq.gz -o $WORKSPACE/fastqc ) ; } 2> $WORKSPACE/"$SAMPLE".log.0.fastqc.log
 
 # Step 1: Map Reads + Sort
-{ time ( minimap2 -t $THREADS -a -x sr $REF_MMI $WORKSPACE/fastq/"$SAMPLE"*.fastq.gz | samtools view -h -F 4 | head -n $NUM_READS_PLUS_3 | samtools sort --threads $THREADS -o $WORKSPACE/"$SAMPLE".sorted.bam ) ; } 2> $WORKSPACE/"$SAMPLE".log.1.map.log
+{ time ( minimap2 -t $THREADS -a -x sr $REF_MMI $WORKSPACE/fastq/"$SAMPLE"*.fastq.gz | samtools sort --threads $THREADS -o $WORKSPACE/"$SAMPLE".sorted.bam ) ; } 2> $WORKSPACE/"$SAMPLE".log.1.map.log
 
 # Step 2: Trim Sorted BAM
 { time ( ivar trim -x 5 -e -i $WORKSPACE/"$SAMPLE".sorted.bam -b $SCRATCH_PRIMER_FP -p $WORKSPACE/"$SAMPLE".trimmed ) ; } > $WORKSPACE/"$SAMPLE".log.2.trim.log 2>&1
