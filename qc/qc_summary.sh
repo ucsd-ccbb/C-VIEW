ee3635b232604fdfb8ad25bbdc574066d6baf87a
AmandaBirmingham <abirmingham@ucsd.edu>
2021-03-25 09:34:31 -0700

    Remove commented out code for manual review folder, change cumulative data to go to a single folder (easier to clean out)

--- qc/qc_summary.sh
+++ qc/qc_summary.sh
@@ -54,16 +54,9 @@ runQC () {
 	# Concatenate all consensus files to a .fas file
 	cat $WORKSPACE/*.consensus.fa > $WORKSPACE/"$SEQ_RUN".fas
 
-    # Id only passing consensus files and write them to a *-passQC.fas file
-    PASSING_CONS_FNAMES=$(python $PIPELINEDIR/qc/subset_csv.py $WORKSPACE/"$SEQ_RUN"-summary.csv not_na_cons_fnames $WORKSPACE)
-    cat $PASSING_CONS_FNAMES > $WORKSPACE/"$SEQ_RUN"-passQC.fas
-
-	# Id only consensus files failing acceptance because of the indel flag.
-	# Write these to a *-indel_flagged.fas file and also create a *-summary.csv
-	# file holding only the records for these sequences
-	# INDEL_CONS_FNAMES=$(python $PIPELINEDIR/qc/subset_csv.py $WORKSPACE/"$SEQ_RUN"-summary.csv indel_flagged_cons_fnames $WORKSPACE)
-	# cat $INDEL_CONS_FNAMES > $WORKSPACE/"$SEQ_RUN"-indel_flagged.fas
-	# python $PIPELINEDIR/qc/subset_csv.py $WORKSPACE/"$SEQ_RUN"-summary.csv filtered_lines indels_flagged True $WORKSPACE/"$SEQ_RUN"-indel_flagged_qc_summary.csv
+  # Id only passing consensus files and write them to a *-passQC.fas file
+  PASSING_CONS_FNAMES=$(python $PIPELINEDIR/qc/subset_csv.py $WORKSPACE/"$SEQ_RUN"-summary.csv not_na_cons_fnames $WORKSPACE)
+  cat $PASSING_CONS_FNAMES > $WORKSPACE/"$SEQ_RUN"-passQC.fas
 
 	# Upload Results
 	echo "Uploading QC and summary results."
@@ -82,10 +75,6 @@ runQC () {
     aws s3 cp $WORKSPACE/"$SEQ_RUN"-summary.csv $QCRESULTS/
 	aws s3 cp $WORKSPACE/"$SEQ_RUN"-acceptance.tsv $QCRESULTS/
 
-	# Manual review folder
-	# aws s3 cp $WORKSPACE/"$SEQ_RUN"-indel_flagged.fas $S3DOWNLOAD/manual_review/
-  #	aws s3 cp $WORKSPACE/"$SEQ_RUN"-indel_flagged_qc_summary.csv $S3DOWNLOAD/manual_review/
-
 	# Tree building data
   if [[ "$ISTEST" = false ]]; then
     S3CUMULATIVE=$S3DOWNLOAD
@@ -94,8 +83,7 @@ runQC () {
   fi
 	aws s3 cp $WORKSPACE/"$SEQ_RUN"-passQC.fas $S3CUMULATIVE/phylogeny/cumulative_data/consensus/
 	aws s3 cp $WORKSPACE/"$SEQ_RUN".fas $S3CUMULATIVE/phylogeny/cumulative_data/consensus/
-	# aws s3 cp $WORKSPACE/"$SEQ_RUN"-summary.acceptance.tsv s3://ucsd-ccbb-projects/2021/20210208_COVID_sequencing/tree_building/acceptance/
-	aws s3 cp $WORKSPACE/"$SEQ_RUN"-summary.csv $S3CUMULATIVE/phylogeny/cumulative_data/qc_summary/
+	aws s3 cp $WORKSPACE/"$SEQ_RUN"-summary.csv $S3CUMULATIVE/phylogeny/cumulative_data/consensus/
 }
 
 { time ( runQC ) ; } > $WORKSPACE/qc/"$SEQ_RUN"-qc_summary.log 2>&1
