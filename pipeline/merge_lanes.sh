d25591b89c26b67756f4e58d2674cd14be877888
AmandaBirmingham <abirmingham@ucsd.edu>
2021-04-09 11:39:35 -0700

    debugging

--- pipeline/merge_lanes.sh
+++ pipeline/merge_lanes.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-#mkdir -p $WORKSPACE/"$SEQ_RUN"_lane_merged_fastq
+mkdir -p $WORKSPACE/"$SEQ_RUN"_lane_merged_fastq
 
 #aws s3 cp $S3DOWNLOAD/ $WORKSPACE/ --recursive --exclude "*" --include "*fastq.gz"
-echo "in merge lanes"
-
 IFS=' ' read -ra SAMPLES_WO_LANE_INFO <<< "$SAMPLES_WO_LANE_INFO_STR"
 IFS=' ' read -ra SAMPLES_W_LANES_COMBINED <<< "$SAMPLES_W_LANES_COMBINED_STR"
 
 # sanity check that arrays are same length
 if [[ ! ${#SAMPLES_WO_LANE_INFO[@]} == ${#SAMPLES_W_LANES_COMBINED[@]} ]] ; then
-   echo "Error: must have same number of sample-without-lane and sample-with-lane-combined entries"
+   echo "Error: must have same number of sample-without-lane and sample-with-lane-combined entries" >> merge.log
    exit 1
 fi
 
@@ -19,11 +17,13 @@ for (( c=1; c<=$END; c++ ))
 do
   SAMPLE=${SAMPLES_WO_LANE_INFO[$i]}
   SAMPLE_W_LANES_COMBINED=${SAMPLES_W_LANES_COMBINED[$i]}
-  echo $SAMPLE
-  echo $SAMPLE_W_LANES_COMBINED
+  echo $SAMPLE >> merge.log
+  echo $SAMPLE_W_LANES_COMBINED >> merge.log
 	#cat $WORKSPACE/"$SAMPLE"*R1_001.fastq.gz > $WORKSPACE/"$SEQ_RUN"_lane_merged_fastq/"$SAMPLE_W_LANES_COMBINED"_R1_001.fastq.gz
 	#cat $WORKSPACE/"$SAMPLE"*R2_001.fastq.gz > $WORKSPACE/"$SEQ_RUN"_lane_merged_fastq/"$SAMPLE_W_LANES_COMBINED"_R2_001.fastq.gz
 done
 
+aws s3 cp $WORKSPACE/"$SEQ_RUN"_lane_merged_fastq/ $S3DOWNLOAD/"$SEQ_RUN"_lane_merged_fastq/ --recursive --exclude "*" --include "*.log"
+
 #aws s3 cp $WORKSPACE/"$SEQ_RUN"_lane_merged_fastq/ $S3DOWNLOAD/"$SEQ_RUN"_lane_merged_fastq/ --recursive --exclude "*" --include "*fastq.gz"
-#rm -rf $WORKSPACE
+rm -rf $WORKSPACE
