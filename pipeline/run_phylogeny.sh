db60e8d64fac282d4c5ec0a8e3ff5cb0dc993168
cfncluster user <ubuntu@ip-10-0-2-103.us-west-2.compute.internal>
2021-04-16 18:10:13 +0000

    input parameters reduced

--- pipeline/run_phylogeny.sh
+++ pipeline/run_phylogeny.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 INPUT=$1 # Sample Sheet with header
-#TIMESTAMP=$(date +'%Y-%m-%d_%H-%M-%S')
 PIPELINEDIR=/shared/workspace/software/covid_sequencing_analysis_pipeline
 S3HELIX=s3://helix-all
 S3UCSD=s3://ucsd-all
 TIMESTAMP=$(date +'%Y-%m-%d_%H-%M-%S')
 
 [ ! -f $INPUT ] && { echo "Error: $INPUT file not found"; exit 99; }
-sed 1d $INPUT | while IFS=',' read ORGANIZATION SEQ_RUN PRIMER_SET FQ MERGE_LANES VARIANTS QC LINEAGE TREE_BUILD ISTEST
+sed 1d $INPUT | while IFS=',' read ORGANIZATION LINEAGE TREE_BUILD ISTEST
 do
 
 	if [[ ! "$ORGANIZATION" =~ ^(ucsd|helix)$ ]]; then
@@ -22,31 +21,6 @@ do
 		S3DOWNLOAD=$S3HELIX
 	fi
 
-	if [[ ! "$PRIMER_SET" =~ ^(artic|swift_v2)$ ]]; then
-		echo "Error: Parameter PRIMER_SET must be one of artic or swift_v2"
-		exit 1
-	fi
-
-	if [[ ! "$FQ" =~ ^(se|pe)$ ]]; then
-	  echo "Error: FQ must be one of se or pe"
-	  exit 1
-	fi
-
-	if [[ ! "$MERGE_LANES" =~ ^(true|false)$ ]]; then
-	  echo "Error: MERGE_LANES must be one of true or false"
-	  exit 1
-	fi
-
-	if [[ ! "$VARIANTS" =~ ^(true|false)$ ]]; then
-	  echo "Error: VARIANTS must be one of true or false"
-	  exit 1
-	fi
-
-	if [[ ! "$QC" =~ ^(true|false)$ ]]; then
-	  echo "Error: QC must be one of true or false"
-	  exit 1
-	fi
-
 	if [[ ! "$LINEAGE" =~ ^(true|false)$ ]]; then
 	  echo "Error: LINEAGE must be one of true or false"
 	  exit 1
@@ -58,13 +32,6 @@ do
 	fi
 
 	echo "Organization: $ORGANIZATION"
-	echo "Seq_Run: $SEQ_RUN"
-	echo "S3 Fastq path: $S3DOWNLOAD/$SEQ_RUN/"$SEQ_RUN"_fastq"
-	echo "Primers: $PRIMER_SET"
-	echo "Fastq Reads: $FQ"
-	echo "Merge Lanes: $MERGE_LANES"
-	echo "Call Variants: $VARIANTS"
-	echo "Run QC: $QC"
 	echo "Lineage with Pangolin: $LINEAGE"
 	echo "Run tree building: $TREE_BUILD"
 	echo "Is test run: $ISTEST"
@@ -77,7 +44,7 @@ do
 		-v ISTEST=$ISTEST \
 		-N tree_building \
 		-wd /shared/workspace/projects/covid/logs \
-		-pe smp 96 \
+		-pe smp 16 \
 		-S /bin/bash \
     	$PIPELINEDIR/pipeline/phylogeny.sh
 
