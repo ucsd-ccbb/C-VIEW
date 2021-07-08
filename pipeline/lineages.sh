00d9f0f9ce164cb44a153608c8cac5de1295fd2f
AmandaBirmingham <abirmingham@ucsd.edu>
2021-07-07 17:05:30 -0700

    temporary debugging additions

--- pipeline/lineages.sh
+++ pipeline/lineages.sh
@@ -25,8 +25,8 @@ if [[ "$SEQ_RUN" != "NA" ]]; then
   INCLUDEMASK="$SEQ_RUN""$INCLUDEMASK"
 fi
 
-echo $EXCLUDEMASK >> $WORKSPACE/"$PROCESSINGID"-phylogeny.exit.log
-echo $INCLUDEMASK >> $WORKSPACE/"$PROCESSINGID"-phylogeny.exit.log
+# echo $EXCLUDEMASK >> $WORKSPACE/"$PROCESSINGID"-phylogeny.exit.log
+# echo $INCLUDEMASK >> $WORKSPACE/"$PROCESSINGID"-phylogeny.exit.log
 
 DOWNLOAD_BUCKETS=()
 if [[ "$ISTEST" == false ]]; then
@@ -46,8 +46,8 @@ else
   S3INSPECT=$S3TEST
 fi
 
-echo $S3UPLOAD >> $WORKSPACE/"$PROCESSINGID"-phylogeny.exit.log
-echo $S3INSPECT >> $WORKSPACE/"$PROCESSINGID"-phylogeny.exit.log
+# echo $S3UPLOAD >> $WORKSPACE/"$PROCESSINGID"-phylogeny.exit.log
+# echo $S3INSPECT >> $WORKSPACE/"$PROCESSINGID"-phylogeny.exit.log
 # echo "${DOWNLOAD_BUCKETS[@]}" >> $WORKSPACE/"$PROCESSINGID"-phylogeny.exit.log
 
 # Actually do the downloads
