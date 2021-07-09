6b3f1a68ac246b2454861818f412144f5545173a
AmandaBirmingham <abirmingham@ucsd.edu>
2021-07-09 10:40:19 -0700

    remove debugging exit

--- pipeline/run_pipeline.sh
+++ pipeline/run_pipeline.sh
@@ -200,9 +200,6 @@ do
 
     echo $(printf '%s\n' "${SAMPLE_LIST[@]}")
 
-    # TODO: remove debugging exit
-    exit 0
-
 		# Run VARIANTS step on each sample
 		for SAMPLE in $SAMPLE_LIST; do
 			qsub $QSUBSAMPLEPARAMS \
