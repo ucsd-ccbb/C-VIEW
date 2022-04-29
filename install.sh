75c809f59ada6f3c5c9d25cabf70322d54053e73
AmandaBirmingham <lists@imladris.com>
2022-04-29 16:41:44 -0700

    updated install script

--- install.sh
+++ install.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# tested on clean ubuntu instance: ami-08962a4068733a2b6
-
-# Provide 1. directory to install software, 2. Directory of anaconda installation ending in anaconda3
-# allow user to specify where to install some software
-SOFTWAREDIR=$1
-ANACONDADIR=$2
-
 # ------ this should happen pre- this script ------
 # install anaconda and python 3.8 using commands below:
 # wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
 # bash Anaconda3-2020.11-Linux-x86_64.sh
 # conda init bash # (and restart shell)
 # ------------------------------------
+
+# tested on clean ubuntu instance: ami-08962a4068733a2b6
+
+SOFTWAREDIR=/shared/workspace/software
+ANACONDADIR=/shared/workspace/software/anaconda3
 
 # ------ Install basic tools ------
 sudo apt-get update
@@ -23,6 +21,9 @@ sudo apt-get install unzip
 sudo apt-get install autoconf
 sudo apt-get install autotools-dev
 sudo apt-get install git
+sudo apt-get install libncurses5
+sudo apt-get install lib32z1
+sudo apt install awscli
 
 # ------- Make software directory ------
 mkdir -p $SOFTWAREDIR
@@ -42,10 +43,10 @@ cd ~
 
 # ------- pi metrics -----------
 wget "https://raw.githubusercontent.com/Niema-Docker/pi_from_pileup/main/pi_from_pileup.cpp"
-g++ -O3 --std=c++11 -o /shared/workspace/software/pi_from_pileup pi_from_pileup.cpp
+g++ -O3 --std=c++11 -o $SOFTWAREDIR/pi_from_pileup pi_from_pileup.cpp
 rm pi_from_pileup.cpp
 
-# ------- cview env from pre-release README on github -------
+# ------- cview env (not: not C-VIEW itself, which was cloned above) -------
 conda create --name cview
 source $ANACONDADIR/bin/activate cview
 conda install numpy
@@ -60,24 +61,11 @@ pip install pandas
 pip install seaborn
 pip install fastaparser
 
-# ------- iVar v.1.3.1 -------
-source $ANACONDADIR/bin/activate cview
-cd $SOFTWAREDIR
-wget https://github.com/andersen-lab/ivar/archive/master.zip -O ivar.zip
-unzip ivar.zip
-mv ivar-master ivar
-rm ivar.zip
-
-# Path to HTSlib from conda (installed by samtools)
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ANACONDADIR/envs/cview/lib
-
-# Install iVar
-cd ivar
-./autogen.sh
-# configure needs to point to location of htslib, LDFLAGS needs to point to location of environment libs
-./configure --with-hts=$ANACONDADIR/envs/cview LDFLAGS=-Wl,-R$ANACONDADIR/envs/cview/lib --prefix=$SOFTWAREDIR/ivar
-make
-make install
+# ------- iVar -------
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda install ivar
 
 # ------- Pangolin -------
 cd $SOFTWAREDIR
