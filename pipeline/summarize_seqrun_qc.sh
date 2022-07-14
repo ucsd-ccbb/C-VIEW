15ae17c68ba5a5268389b8951c01b8fe2cd89ad1
AmandaBirmingham <lists@imladris.com>
2022-07-14 16:59:00 -0700

    correct unintended change of qc log file suffix from -qc.exit.log to -src.exit.log

--- pipeline/summarize_seqrun_qc.sh
+++ pipeline/summarize_seqrun_qc.sh
@@ -48,7 +48,7 @@ runQC () {
 	# mv depth_lineplot.pdf $WORKSPACE/src/"$SEQ_RUN"-depth_lineplot.pdf
 	echo "Summarizing consensus QC."
 	python $CVIEWDIR/src/seq_run_acceptance.py $WORKSPACE $WORKSPACE/"$SEQ_RUN"-acceptance.tsv
-    echo -e "seq_run_acceptance.py exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-src.exit.log
+    echo -e "seq_run_acceptance.py exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-qc.exit.log
 
 	# Multiqc
 	echo "Configuring Multiqc"
@@ -57,33 +57,33 @@ runQC () {
 	find $WORKSPACE -name "*_subsampled_mapping_stats.tsv" | sort -n > $WORKSPACE/src/subsampled_mapping_stats_paths.txt
 
 	python $CVIEWDIR/src/custom_gen_stats_multiqc.py $WORKSPACE/src/qualimapReport_paths.txt $WORKSPACE/src/q30_reads_paths.txt $WORKSPACE/src/subsampled_mapping_stats_paths.txt $FQ $WORKSPACE/multiqc_custom_gen_stats.yaml
-  echo -e "custom_gen_stats_multiqc.py exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-src.exit.log
+  echo -e "custom_gen_stats_multiqc.py exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-qc.exit.log
 
 	cat $CVIEWDIR/src/cview_multiqc_config.yaml $WORKSPACE/multiqc_custom_gen_stats.yaml > $WORKSPACE/src/"$SEQ_RUN"-custom_gen_stats_config.yaml
 	multiqc --config $WORKSPACE/src/"$SEQ_RUN"-custom_gen_stats_config.yaml --module qualimap --module custom_content $WORKSPACE
-    echo -e "multiqc exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-src.exit.log
+    echo -e "multiqc exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-qc.exit.log
 
     # Concatenate coverage files
     echo "Concatenating coverage files"
     echo -e "SAMPLE\tCOVERAGE\tAVG_DEPTH\tMIN\tMAX\tZERO_DEPTH" > $WORKSPACE/"$SEQ_RUN"-coverage.tsv
     cat $WORKSPACE/*/*coverage.tsv | sort -n -k 2 >> $WORKSPACE/"$SEQ_RUN"-coverage.tsv
-    echo -e "coverage cat exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-src.exit.log
+    echo -e "coverage cat exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-qc.exit.log
 
   # Concatenate per-sample files for per-sample metrics
   for PER_SAMPLE_METRIC in trimmed_bam_read_count n_metric pi_metric ; do
     echo "Concatenating $PER_SAMPLE_METRIC files"
     echo -e "sequenced_pool_component_id\t$PER_SAMPLE_METRIC" > $WORKSPACE/"$SEQ_RUN"-"$PER_SAMPLE_METRIC".tsv
     cat $WORKSPACE/*/*"$PER_SAMPLE_METRIC".tsv | sort -n -k 2 >> $WORKSPACE/"$SEQ_RUN"-"$PER_SAMPLE_METRIC".tsv
-    echo -e "$PER_SAMPLE_METRIC cat exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-src.exit.log
+    echo -e "$PER_SAMPLE_METRIC cat exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-qc.exit.log
   done
 
 	# Make summary table
 	echo "Making run summary table."
 	# TODO: would be nice to use the same list of per-sample metrics here as above to avoid having to remember to add new ones in two places
 	python $CVIEWDIR/src/seq_run_summary.py $WORKSPACE/"$SEQ_RUN"-temp-summary.csv $WORKSPACE/multiqc_data/multiqc_general_stats.txt $WORKSPACE/"$SEQ_RUN"-acceptance.tsv $WORKSPACE/"$SEQ_RUN"-trimmed_bam_read_count.tsv $WORKSPACE/"$SEQ_RUN"-pi_metric.tsv $WORKSPACE/"$SEQ_RUN"-n_metric.tsv
-    echo -e "seq_run_summary.py exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-src.exit.log
+    echo -e "seq_run_summary.py exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-qc.exit.log
 	python $CVIEWDIR/src/integrate_bjorn_coverage.py $WORKSPACE/"$SEQ_RUN"-temp-summary.csv $WORKSPACE/"$SEQ_RUN"-coverage.tsv $WORKSPACE/"$SEQ_RUN"-summary.csv
-    echo -e "integrate_bjorn_coverage.py exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-src.exit.log
+    echo -e "integrate_bjorn_coverage.py exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-qc.exit.log
 
 	# Concatenate all consensus files to a .fas file
 	cat $WORKSPACE/*.consensus.fa > $WORKSPACE/"$SEQ_RUN".fas
@@ -91,19 +91,19 @@ runQC () {
     # Id only passing consensus files and write them to a *-passQC.fas file
     echo "Merging passing consensus files."
     PASSING_CONS_FNAMES=$(python $CVIEWDIR/src/subset_csv.py $WORKSPACE/"$SEQ_RUN"-summary.csv not_na_cons_fnames $WORKSPACE)
-    echo -e "subset_csv.py exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-src.exit.log
+    echo -e "subset_csv.py exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-qc.exit.log
     cat $PASSING_CONS_FNAMES > $WORKSPACE/"$SEQ_RUN"-passQC.fas
 
   # generate file of summary and passQC fas checksums, for record-keeping
   python $CVIEWDIR/src/document_file_checksums.py \
     $WORKSPACE $WORKSPACE/"$SEQ_RUN"_artifact_checksums.csv \
     "$SEQ_RUN-summary.csv" "$SEQ_RUN-passQC.fas"
-  echo -e "document_file_checksums.py exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-src.exit.log
+  echo -e "document_file_checksums.py exit code: $?" >> $WORKSPACE/"$SEQ_RUN"-qc.exit.log
 
 	# Exit codes
 	echo "Gathering per-sample exit codes."
 	cat $WORKSPACE/*/*error.log > $WORKSPACE/"$SEQ_RUN".error.log
-	grep -v "exit code: 0" $WORKSPACE/"$SEQ_RUN"-src.exit.log | head -n 1 >> $WORKSPACE/"$SEQ_RUN".error.log
+	grep -v "exit code: 0" $WORKSPACE/"$SEQ_RUN"-qc.exit.log | head -n 1 >> $WORKSPACE/"$SEQ_RUN".error.log
 
 	# Upload Results
 	echo "Uploading multiqc and qc-subfolder results."
